be30ca053152877925b34b5fbb60d0337470d252
Manuel Orellana <Manuel Orellana@DESKTOP-MANUEL>
2018-08-31 16:08:13 -0600

    Modificacion

--- src/main/java/sv/edu/udb/pojo/Venta.java
+++ src/main/java/sv/edu/udb/pojo/Venta.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package sv.edu.udb.pojo;
 
 import java.sql.Date;
@@ -66,9 +62,5 @@ public class Venta {
     public void setEstado(String estado) {
         this.estado = estado;
     }
-    
-    
-    
-    
-    
+       
 }
