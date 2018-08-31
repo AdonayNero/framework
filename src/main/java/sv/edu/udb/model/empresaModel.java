7bf3df0e35d27c88812bbb8cca283bdbb0eab920
Edwin Bonilla <Edwin Bonilla@EdwinBonilla-PC>
2018-08-31 11:57:41 -0600

    modificaciones empresa

---
+++ src/main/java/sv/edu/udb/model/empresaModel.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sv.edu.udb.model;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import sv.edu.udb.pojo.Empresa;
+
+/**
+ *
+ * @author Edwin Bonilla
+ */
+public class empresaModel extends Conexion{
+    
+    public int insertar(Empresa emp) throws SQLException{
+        
+        int filasAffec = 0;
+        String sql = "INSERT INTO `empresa`(`nombreEmp`, `Estado`, `Porcentaje`) VALUES (?,?,?)";
+        this.conectar();
+        st = conexion.prepareStatement(sql);
+        st.setString(1, emp.getNombre());
+        st.setString(2, emp.getEstado());
+        st.setInt(3, emp.getPorcentaje());
+        
+        filasAffec = st.executeUpdate();
+        this.desconectar();
+        return filasAffec;
+    }
+    
+    public List<Empresa> listar() throws SQLException{
+        Empresa emp = null;
+        List<Empresa> empresas = new ArrayList<>();
+        
+        String sql = "SELECT `idEmpresa`, `nombreEmp`, `Estado`, `Porcentaje` FROM `empresa`";
+        this.conectar();
+        st = conexion.prepareStatement(sql);
+        rs = st.executeQuery();
+        
+        while (rs.next()) {
+            emp = new Empresa();
+            emp.setId(rs.getInt(1));
+            emp.setNombre(rs.getString(2));
+            emp.setEstado(rs.getString(3));
+            emp.setPorcentaje(rs.getInt(4));
+            
+            empresas.add(emp);
+        }
+        this.desconectar();
+        return empresas;
+    }
+    
+    public int eliminar(int id) throws SQLException{
+        int filasAffec = 0;
+        
+        String sql = "DELETE FROM `empresa` WHERE id = ?";
+        this.conectar();
+        st = conexion.prepareStatement(sql);
+        filasAffec = st.executeUpdate();
+        this.desconectar();
+        return filasAffec;
+        
+    }
+    
+    
+}
